98bbab2a38b7caf65ca5c9c9feb3a1a15ef8034c
Amir Burbea <amir.burbea@gmail.com>
2022-09-12 18:11:26 -0700

    Update

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react';
+import './styles.css';
+
+export type AppProps = {};
+
+export const App: FunctionComponent<AppProps> = () => (
+  <div className="App">
+    <span>Hello World</span>
+  </div>
+);
